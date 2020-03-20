ba114b6584c37b3c826e8b9021d68ba3ed38a4ae
hamo <allthatjazz@outlook.com>
2020-03-20 14:28:46 -0400

    Gatsby setup

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Ques1 from './question1';
+
+import Layout from "../components/layout"
+
+const IndexPage = () => (
+  <Layout>
+    <Ques1 />
+  </Layout>
+)
+
+export default IndexPage
---
+++ src/pages/question1.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Q1 = () => (
+  <div>Question 1</div>
+)
+
+export default Q1
